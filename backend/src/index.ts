b7e8a478a2dbf40ace2e2883d99452714c9cb20a
kibartas <juodasis.perlas@gmail.com>
2020-09-25 13:21:03 +0300

    backend-feature: implement POST endpoint for '/api/exchange'. User is now able to send requests to this endpoint and get information about the exchange

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,6 +29,32 @@ app.get('/list', async (req: Request, res: Response) => {
   res.send(response);
 })
 
+app.post('/api/exchange', validateRequest);
+
+app.post('/api/exchange', async (req: Request, res: Response): Promise<void> => {
+  type IResponse = {
+    fromAmt: string;
+    toAmt: string;
+    fromCur: string;
+    toCur: string;
+  };
+  const body: IRequestBody = req.body;
+  const twoFxRates: IFxRate[] = await findFxRates(body.ccy_from, body.ccy_to);
+  // find index of the fxRate that is being converted to
+  const i = twoFxRates.findIndex((fxRate) => fxRate.ccy_to === body.ccy_to);
+  const j = i === 0 ? 1 : 0;
+  let fromAmt = body.amt.toString();
+  if (fromAmt.includes('.'))
+    fromAmt = fromAmt.charAt(fromAmt.length - 2) === '.' ? fromAmt + '0' : fromAmt;
+  const response: IResponse = {
+    fromAmt: fromAmt,
+    toAmt: convertMoney(twoFxRates[j].to_amt, twoFxRates[i].to_amt, body.amt as unknown as number).toFixed(2),
+    fromCur: body.ccy_from,
+    toCur: body.ccy_to
+  }
+  res.send(response);
+});
+
 const port = process.env.port || 8080;
 
 app.listen(port, async () => {
